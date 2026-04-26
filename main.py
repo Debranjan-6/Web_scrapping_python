10b41f9c27c13be96dfba3f9b0f884b55ebf32f7
Debranjan-6 <debchattaraj6@gmail.com>
2026-04-26 18:06:20 +0530

    To remove extra #

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ webbrowser.open("https://www.google.com/maps/place/"+address)
    
 
     #print("argv 0="+sys.argv[0])
-    #print("argv 1="+sys.argv[1])#
+    #print("argv 1="+sys.argv[1])
     #print("argv 2="+sys.argv[2])
     #print("argv 3="+sys.argv[3])
 
