34568161db100d4597f491d0614448cc00ea26d1
Debranjan-6 <debchattaraj6@gmail.com>
2026-04-26 18:00:33 +0530

    Add Map functionality

---
+++ main.py
@@ -0,0 +1,24 @@
+import webbrowser
+import sys
+import pyperclip
+
+#webbrowser.open('http://inventwithpython.com/')
+
+
+if len(sys.argv) > 1:
+# Get address from command line.
+    address = ' '.join(sys.argv[1:])
+    print ("Address = "+address)
+else:
+    # Get address from clipboard.
+        address = pyperclip.paste()
+webbrowser.open("https://www.google.com/maps/place/"+address)
+   
+
+    #print("argv 0="+sys.argv[0])
+    #print("argv 1="+sys.argv[1])#
+    #print("argv 2="+sys.argv[2])
+    #print("argv 3="+sys.argv[3])
+
+    
+
