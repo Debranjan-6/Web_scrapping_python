2a17b6359dcc9767ef0e22adb0999e474ce99aad
Debranjan-6 <debchattaraj6@gmail.com>
2026-04-27 20:52:41 +0530

    Get Contain

---
+++ get_content.py
@@ -0,0 +1,10 @@
+import requests
+res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+print(type(res))
+print(res.status_code == requests.codes.ok)
+
+
+
+print("length="+str (len(res.text)))
+
+print(res.text[:250])
